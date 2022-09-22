0db626fbd406cb15a02579a34221e424dbd9635f
Senh Linsh <lyuanxiang0812@126.com>
2022-09-22 16:30:27 +0800

    ✨ 优化 ITextItemView 实现

--- components/src/main/java/com/linsh/view/impl/INormalTextItemViewImpl.java
+++ components/src/main/java/com/linsh/view/impl/INormalTextItemViewImpl.java
@@ -6,11 +6,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+
+import com.linsh.lshutils.utils.Dps;
 import com.linsh.utilseverywhere.ResourceUtils;
 import com.linsh.view.item.ITextItemView;
 import com.linsh.views.R;
-
-import androidx.annotation.NonNull;
 
 /**
  * <pre>
@@ -25,8 +26,11 @@ class INormalTextItemViewImpl implements ITextItemView {
     private final TextView textView;
 
     public INormalTextItemViewImpl(Context context) {
+        ViewGroup.MarginLayoutParams layoutParams = new ViewGroup.MarginLayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        int margin = Dps.toPx(1);
+        layoutParams.setMargins(margin, margin, margin, margin);
         textView = new TextView(context);
-        textView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        textView.setLayoutParams(layoutParams);
         int padding = ResourceUtils.getDimens(R.dimen.lsh_item_padding);
         textView.setPadding(padding, padding, padding, padding);
         int textSize = ResourceUtils.getDimens(R.dimen.lsh_item_text_size);
